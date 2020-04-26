52cfbd9c968172c449c4425ff45d31a89b223eba
Elvis A. Gastelum <43228550+ElvisGastelum@users.noreply.github.com>
2020-04-26 02:39:42 -0700

    Infrastructure as code first commit

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {}
+variable "website_domain" {}
+variable "website_subdomain" {}
+variable "website_bucket_zone" {}
+variable "website_bucket_name" {}
